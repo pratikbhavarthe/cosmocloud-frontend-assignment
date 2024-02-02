3a2bc2ca22ed42303cc31fc1f34cd74d723d3d79
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-02-02 23:16:12 +0530

    initialize React and React Flow providers for App

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ReactFlowProvider } from 'reactflow'
+import { NodeProvider } from './context/NodeContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ReactFlowProvider>
+      <NodeProvider>
+        <App />
+      </NodeProvider>
+    </ReactFlowProvider>
   </React.StrictMode>,
 )
